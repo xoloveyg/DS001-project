50fa654221396932beb31aca23ffe8c928ac6c53
xoloveyg <93149792+xoloveyg@users.noreply.github.com>
2022-08-19 11:27:25 +0430

    Add files via upload

---
+++ CommonTypes.py
@@ -0,0 +1,14 @@
+class CommonTypes:
+    HOME = 0
+    INTERSECTIONS = 1
+    Officers = 2
+    addIntersection = 3
+    searchInterSection = 4
+    CHANGE_LIGHT_INTERSECTION = 5
+    SET_TIMER_INTERSECTION = 6
+    ADD_OFFICER = 7
+    SET_SHIFT_OFFICER = 8
+    SEARCH_OFFICER = 9
+    SHOW_SHIFT_OFFICER = 10
+    ATTENDANCE = 11
+    EXIT = 12
